d02a171ea50c075b077aca49ab59d7f763e2331e
Christopher Fajardo <chris.fajardo@tri.global>
2021-07-16 12:17:11 -0700

    fix pressure type, no Optional fields, add descriptions to show up on api

--- web/backend/app/api.py
+++ web/backend/app/api.py
@@ -6,7 +6,7 @@ from app.services import route_recommendation_service
 router = fastapi.APIRouter()
 
 
-@router.post("/api/recommend_routes", description="generated recommended routes for given material")
+@router.post("/api/recommend_routes", description="generate recommended routes for given material")
 def recommend_routes(form: RecommendRoutesForm):
     return fastapi.responses.Response(
         content=route_recommendation_service(form).to_json(),
