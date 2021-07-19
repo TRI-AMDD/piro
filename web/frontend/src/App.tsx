a95e1f6e210039d2c7b307d2a20ca0548f139dcc
Mike Puzon <michael.puzon@tri.global>
2021-07-19 16:47:03 -0700

    Remove tri-web-app demo code

--- web/frontend/src/App.tsx
+++ web/frontend/src/App.tsx
@@ -4,7 +4,6 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 
 const queryClient = new QueryClient();
 const Home = lazy(() => import('./pages/Home/Home'));
-const Demo = lazy(() => import('./pages/Demo/Demo'));
 
 const App = () => (
     <QueryClientProvider client={queryClient}>
@@ -12,7 +11,6 @@ const App = () => (
             <Suspense fallback={<div>Loading...</div>}>
                 <Switch>
                     <Route exact path="/" component={Home} />
-                    <Route path="/demo" component={Demo} />
                 </Switch>
             </Suspense>
         </Router>
