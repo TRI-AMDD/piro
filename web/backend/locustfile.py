9d3b051e5429a98a879ac7dc9c25210be35df300
Christopher Fajardo <chris.fajardo@tri.global>
2021-09-10 12:59:35 -0700

    add example requests from Murat and profile all of them

--- web/backend/locustfile.py
+++ web/backend/locustfile.py
@@ -1,44 +1,46 @@
 import time
+import random
 
 from locust import HttpUser, task, tag, events
 
-from app.models import RecommendRoutesRequest, RecommendRoutesTaskStatus
+from app.models import RecommendRoutesTaskStatus
+from example_requests import requests
 
 
 class SynthesisAppUser(HttpUser):
+    """
+    locust -H http://localhost -f locustfile.py -u 1 -r 1 --autostart
+
+    # sync only
+    locust --tag sync -H http://localhost -f locustfile.py -u 1 -r 1 --autostart
+
+    # async only
+    locust --tag async -H http://localhost -f locustfile.py -u 1 -r 1 --autostart
+    """
 
     @tag('sync')
     @task
     def sync_run(self):
-        request = RecommendRoutesRequest(
-            target_entry_id='mp-9029',
-            temperature=1600,
-            pressure=0.001,
-            max_component_precursors=2,
-            show_fraction_known_precursors=False,
-            show_known_precursors_only=False,
-            display_peroxides=True,
-            add_pareto=True
-        )
+        request_dict = random.choice(list(requests.values()))
 
-        self.client.post('/api/recommend_routes', json=request.dict())
+        start_time = time.time() * 1000
+        self.client.post('/api/recommend_routes', json=request_dict)
+        end_time = time.time() * 1000
+
+        events.request_success.fire(
+            request_type="task",
+            name=f'{request_dict["target_entry_id"]} sync_run',
+            response_time=end_time - start_time,
+            response_length=0,
+        )
 
     @tag('async')
     @task
     def async_run(self):
-        request = RecommendRoutesRequest(
-            target_entry_id='mp-9029',
-            temperature=1600,
-            pressure=0.001,
-            max_component_precursors=2,
-            show_fraction_known_precursors=False,
-            show_known_precursors_only=False,
-            display_peroxides=True,
-            add_pareto=True
-        )
+        request_dict = random.choice(list(requests.values()))
 
         start_time = time.time() * 1000
-        resp = self.client.post('/api/recommend_routes_task', json=request.dict())
+        resp = self.client.post('/api/recommend_routes_task', json=request_dict)
         query = f'/api/recommend_routes_task/{resp.json()["task_id"]}'
 
         status = RecommendRoutesTaskStatus.INVALID
@@ -50,7 +52,7 @@ class SynthesisAppUser(HttpUser):
 
         events.request_success.fire(
             request_type="task",
-            name="async_run",
+            name=f'{request_dict["target_entry_id"]} async_run',
             response_time=end_time - start_time,
             response_length=0,
         )
