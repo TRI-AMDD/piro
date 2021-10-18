8cc7ec64c0981499b6b4decabe9320de9c5650b9
Mike Puzon <michael.puzon@tri.global>
2021-10-18 12:21:15 -0700

    Fetch jwt token on user sign in

--- web/frontend/src/pages/Home/plotDataContext.tsx
+++ web/frontend/src/pages/Home/plotDataContext.tsx
@@ -1,6 +1,6 @@
 import React, { FC, createContext, useContext, useState, useMemo, useEffect } from 'react';
 import { UseMutationResult } from 'react-query';
-import { Auth } from 'aws-amplify';
+import { Auth, Hub } from 'aws-amplify';
 import { useSubmitTask, useNormalPlotData } from './usePlotData';
 
 type ContextProps = {
@@ -16,8 +16,17 @@ const PlotDataProvider: FC = ({ children }) => {
     const [apiMode, setApiMode] = useState('task');
     const [token, setToken] = useState('');
 
-    // fetch the token to be used for api calls
+    // fetch the token to be used for api calls when user has signed in or up
     useEffect(() => {
+        Hub.listen('auth', (data) => {
+            const event = data.payload.event;
+            if (event === 'signIn' || event === 'signUp') {
+                Auth.currentSession()
+                    .then((session) => { setToken(session.getIdToken().getJwtToken()) })
+                    .catch(() => {})
+            }
+        })
+
         Auth.currentSession()
             .then((session) => { setToken(session.getIdToken().getJwtToken()) })
             .catch(() => {})
