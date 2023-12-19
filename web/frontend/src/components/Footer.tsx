994aad8ee71a092d5d7904ee6ed24f5e6500e604
Nadimpalli Varma (Contractor) <nadimpalli.varma.ctr@tri.global>
2023-12-19 10:19:28 +0530

    added image in header

--- web/frontend/src/components/Footer.tsx
+++ web/frontend/src/components/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
         <footer className={styles.Footer}>
             <p><Link  className={styles.Footerlink} to="/about">About</Link></p>
             <p><Link className={styles.Footerlink} to="/about">Code</Link></p>
-            <p><Link className={styles.Footerlink} to="/about">manuscript</Link></p>
+            <p><Link className={styles.Footerlink} to="/about">Manuscript</Link></p>
             <p ><a className={styles.Footerlink} href="https://www.tri.global/privacy-policy/" target="_blank" rel="noreferrer">Privacy Policy</a></p>
             <p className={styles.Footercopyright}>© Copyright Toyota Research Institute {year}</p>
         </footer>
--- web/frontend/src/components/Header.tsx
+++ web/frontend/src/components/Header.tsx
@@ -1,15 +1,17 @@
 import { AmplifySignOut } from '@aws-amplify/ui-react';
 import styles from './components.module.css';
-
+import logo from '../images/blackimage.png'
 
 export default function Header() {
+    //var logo = require('../images/Toyota_Research_Institute_Logo.png')
+    console.log(logo)
     return (
         <header className={styles.AppHeader}>
+            <img src={logo} alt="Logo" width={"150px"}/>
             <h3>Piro Synthesis Analyzer</h3>
             <a href="/about" className={styles.Aboutlink}>About</a>
-            <button className={styles.Signout}>Sign Out</button>
+            <button className={styles.Signout}>SIGN OUT</button>
             {AMPLIFY_ENABLED && <AmplifySignOut />}
-            
         </header>
     );
 }
