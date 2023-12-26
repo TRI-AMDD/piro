84d61b40bb8da5e29b44a4863d2e050188834834
shilpasivanesan-tri <shilpa.sivanesan.ctr@tri.global>
2023-12-26 23:28:18 +0530

    maintaing versions for few pacakges as mentioned in main

--- tests/unit_tests/test_routes.py
+++ tests/unit_tests/test_routes.py
@@ -2,7 +2,7 @@ import os
 import unittest
 import warnings
 from pathlib import Path
-from pymatgen.ext.matproj import MPRester, MPRestError, MPRester
+from pymatgen.ext.matproj import MPRestError, MPRester
 from pymatgen.core import SETTINGS
 from _pytest.monkeypatch import MonkeyPatch
 from monty.serialization import loadfn, dumpfn
