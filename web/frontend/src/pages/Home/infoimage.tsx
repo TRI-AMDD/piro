58dea919513811c01ebc3ac798694fa868098a73
Nadimpalli Varma (Contractor) <nadimpalli.varma.ctr@tri.global>
2024-02-12 14:18:40 +0530

    added a two line gap in tool tip for the sentences

--- web/frontend/src/pages/Home/infoimage.tsx
+++ web/frontend/src/pages/Home/infoimage.tsx
@@ -17,12 +17,20 @@ const InfoImage: React.FC<InfoImageProps> = ({ imagePath, altText, information }
   const handleMouseLeave = () => {
     setTooltipVisible(false);
   };
-
-  const renderParagraphs = () => {
-    const paragraphs = information.split('<br>');
-
-    return paragraphs.map((paragraph, index) => <p key={index}>{paragraph}</p>);
-  };
+  const characterToColor: string = "`";
+  const lines: string[] = information.split("<br>");
+  const modifiedLines: JSX.Element[] = lines.map((line: string, lineIndex: number) => {
+    const characters: string[] = line.split("");
+    const modifiedCharacters: JSX.Element[] = characters.map((char: string, index: number) => {
+      if (char === characterToColor) {
+        return <span key={index} style={{ color: '#222020' }}>{char}</span>;
+      } else {
+        return <span key={index}>{char}</span>;
+      }
+    });
+    const modifiedLine: JSX.Element = <>{modifiedCharacters}</>;
+    return <div key={lineIndex}>{modifiedLine}</div>;
+  });
 
   return (
     <div className={styles.container}>
@@ -36,7 +44,7 @@ const InfoImage: React.FC<InfoImageProps> = ({ imagePath, altText, information }
 
       {isTooltipVisible && (
         <div className={styles.tooltip}>
-          <div className={styles.tooltiptext}>{renderParagraphs()}</div>
+          <div className={styles.tooltiptext}>{modifiedLines}</div>
         </div>
       )}
     </div>
