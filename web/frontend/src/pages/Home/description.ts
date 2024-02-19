15e821c82404d7fc68469a89479f7e07187484b0
Nadimpalli Varma (Contractor) <nadimpalli.varma.ctr@tri.global>
2024-02-19 10:57:00 +0530

    text change

--- web/frontend/src/pages/Home/description.ts
+++ web/frontend/src/pages/Home/description.ts
@@ -5,7 +5,7 @@ export const description = {
   flexible_competition:
     'Used to limit the reactants to simpler sub-chemistries<br> of our target to obtain a refined precursor list.<br>`<br> For example,setting this as 2 for a ternary target compound would limit precursors <br> to binary compounds<br> (additional element count ignored)',
   hull_distance:
-    'Deselect “Stable precursors only” checkbox<br> if you wish to edit “Distance to Hull”<br>`<br>This defines the energy range of metastable materials<br> for inclusion for inclusion in precursor library.',
+    'Deselect “Stable precursors only” checkbox<br> if you wish to edit “Distance to Hull”.<br>`<br>This defines the energy range of metastable materials<br> for inclusion for inclusion in precursor library.',
   add_elements: 'For example, to include carbonates and carbides, select C here.',
   explicit_includes: 'List of Materials Project IDs of additional materials to include in precursor list',
   allow_gas_release: 'Reactions are balanced such that O2, CO2 etc. can be released alongside the target',
