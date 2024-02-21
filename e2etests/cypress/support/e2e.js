22fabfdcac9164e42ce123b66930bd4aba2aaaec
Harisilpa Davanam <harisilpa.davanam.ctr@tri.global>
2024-02-21 15:49:32 +0530

    removed the e2etests  folder

--- e2etests/cypress/support/e2e.js
+++
@@ -1,3 +0,0 @@
-// cypress/support/{scheme}.js, where {scheme} defaults to e2e
-const compareSnapshotCommand = require('cypress-image-diff-js/command');
-compareSnapshotCommand();
--- e2etests/cypress/support/index.js
+++
@@ -1,3 +0,0 @@
-after(() => {
-    cy.task('generateReport')
-    })
