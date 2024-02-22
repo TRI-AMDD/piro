f6d9a68be28c41878f6413d6bf3468a2272c2df3
Gobuviknesh Jayabalan (Contractor) <gobuviknesh.jaya.ctr@U-PDFMFFG9MM6.internal.tri.global>
2024-02-22 16:35:34 +0530

    fixed lint errors

--- web/frontend/src/pages/Home/AdvancedOptions.tsx
+++ web/frontend/src/pages/Home/AdvancedOptions.tsx
@@ -1,6 +1,5 @@
 import { Dispatch, SetStateAction, useEffect } from 'react';
 import { Control, UseFormRegister } from 'react-hook-form';
-import { Button } from '@material-tailwind/react';
 import { useState } from 'react';
 import styles from './Home.module.css';
 import FormCheckbox from './Checkbox';
@@ -12,12 +11,10 @@ import { MultiValue } from 'react-select';
 import InfoImage from './infoimage';
 import logo from './info.svg';
 import { infoHandleHover } from 'src/utils/GA';
-import Collapse from "@mui/material/Collapse";
-import KeyboardArrowDownIcon from
-    "@mui/icons-material/KeyboardArrowDown";
-import KeyboardArrowUpIcon from
-    "@mui/icons-material/KeyboardArrowUp";
-import IconButton from "@mui/material/IconButton";
+import Collapse from '@mui/material/Collapse';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import IconButton from '@mui/material/IconButton';
 
 interface Props {
   control: Control<Inputs>;
@@ -65,12 +62,12 @@ const toggleOptions = [
   { label: 'Normal Route', value: 'normal' }
 ];
 
-const AdvancedOptions = (props) => {
+const AdvancedOptions = (props: Props) => {
   const { control, register, setExcludeCompositions, compoundMode } = props;
   const { apiMode, setApiMode } = usePlotData();
-  const collapsibleStyle = { display: "flex", justifyContent: "space-between", width:"100%"};
-  const iconStyle = { bottom : "5px"};
-  const lineStyle = { borderTop: "1px solid #cccccc"};
+  const collapsibleStyle = { display: 'flex', justifyContent: 'space-between', width: '100%' };
+  const iconStyle = { bottom: '5px' };
+  const lineStyle = { borderTop: '1px solid #cccccc' };
   const [open, setOpen] = useState<boolean>(false);
   useEffect(() => {
     if (compoundMode === 'cif') {
@@ -78,124 +75,120 @@ const AdvancedOptions = (props) => {
     }
   }, [compoundMode, setApiMode]);
 
-
-
-
-
-
- return (
-<div>
- <div className={styles.advancetext} style = {collapsibleStyle}>Advanced Options
-  <IconButton onClick={() => setOpen(!open)}  aria-label="expand" size="small" style = {iconStyle}>
-  {open ? <KeyboardArrowUpIcon />: <KeyboardArrowDownIcon />}</IconButton></div>
-  <div style={lineStyle}></div>
-                 <div className={styles.advanceoptionsformgrid}>
-                     <Collapse in={open} timeout="auto"
-                         unmountOnExit>
-                             <div className={styles.advanceoptionsformgrid}>
-                             <div className={styles.advancedoptionscontent}>
-                                   {compoundMode === 'compound' && (
-                                       <RadioToggle
-                                          options={isProd ? toggleOptions.filter((itr) => itr.value != 'task') : toggleOptions}
-                                          onChange={setApiMode}
-                                          value={isProd ? 'normal' : apiMode}
-                                        />
-                                               )}
-                                               <div className={styles.selectCSS}>
-                                                   <div>
-                                                      <div
-                                                         className={styles.labelwithinfo}
-                                                         onMouseOver={() => infoHandleHover('infoHover', description.simple_precursors)}
-                                                       >
-                                                           <label className={styles.label}>Use Simplified Precursor Library</label>
-                                                           <InfoImage imagePath={logo} altText="Info" information={description.simple_precursors} />
-                                                       </div>
-                                                       <input
-                                                           className={styles.inputfield}
-                                                           type="number"
-                                                           step="any"
-                                                           defaultValue={0}
-                                                           {...register('simple_precursors', { valueAsNumber: true })}
-                                                        />
-                                                       </div>
-                                                   </div>
-                                                    <div className={styles.selectCSS}>
-                                                        <div>
-                                                           <div className={styles.labelwithinfo}>
-                                                              <label className={styles.label}>Surface energy scaling factor</label>
-                                                           </div>
-                                                           <input
-                                                               className={styles.inputfield}
-                                                                                         type="number"
-                                                                                         step="any"
-                                                                                         defaultValue={0.12484}
-                                                                                         {...register('sigma', { valueAsNumber: true })}
-                                                                                       />
-                                                                                     </div>
-                                                                                   </div>
-                                                                                   <div className={styles.selectCSS}>
-                                                                                     <div>
-                                                                                       <div className={styles.labelwithinfo}>
-                                                                                         <label className={styles.label}>Transport barrier</label>
-                                                                                       </div>
-                                                                                       <input
-                                                                                         className={styles.inputfield}
-                                                                                         type="number"
-                                                                                         step="any"
-                                                                                         defaultValue={10}
-                                                                                         {...register('transport_constant', { valueAsNumber: true })}
-                                                                                       />
-                                                                                     </div>
-                                                                                   </div>
-                                                                                   <div className={styles.selectCSS}>
-                                                                                     <div>
-                                                                                       <div
-                                                                                         className={styles.labelwithinfoforselect}
-                                                                                         onMouseOver={() => infoHandleHover('infoHover', description.exclude_compositions)}
-                                                                                       >
-                                                                                         <label className={styles.label}>Formulas to filter out</label>
-                                                                                         <InfoImage imagePath={logo} altText="Info" information={description.exclude_compositions} />
-                                                                                       </div>
-                                                                                       <MultiSelect placeholder="Formulas to filter out" setValues={setExcludeCompositions} />
-                                                                                     </div>
-                                                                                   </div>
-                                                                                 </div>
-                                                                                 <div className={styles.Checkboxes}>
-                                                                                   <div>
-                                                                                     <div
-                                                                                       className={styles.labelwithinfo}
-                                                                                       onMouseOver={() => infoHandleHover('infoHover', description.confine_competing_to_icsd)}
-                                                                                     >
-                                                                                       <FormCheckbox name="confine_competing_to_icsd" control={control} defaultValue={false} />
-                                                                                       <label className={styles.checklabel}>ICSD-based parasitic phases only</label>
-                                                                                       <InfoImage imagePath={logo} altText="Info" information={description.confine_competing_to_icsd} />
-                                                                                     </div>
-                                                                                   </div>
-                                                                                   <div>
-                                                                                     <div className={styles.labelwithinfo}>
-                                                                                       <FormCheckbox name="display_peroxides" control={control} defaultValue={true} />
-                                                                                       <label> Show reactions involving peroxides</label>
-                                                                                     </div>
-                                                                                   </div>
-                                                                                   <div>
-                                                                                     <div className={styles.labelwithinfo}>
-                                                                                       <FormCheckbox name="display_superoxides" control={control} defaultValue={false} />
-                                                                                       <label> Show reactions involving superoxides</label>
-                                                                                     </div>
-                                                                                   </div>
-                                                                                   <div>
-                                                                                     <div className={styles.labelwithinfo}>
-                                                                                       <FormCheckbox name="add_pareto" control={control} defaultValue={true} />
-                                                                                       <label> Show the Pareto front</label>
-                                                                                     </div>
-                                                                                   </div>
-                                                                                 </div>
-
-                             </div>
-                     </Collapse>
-                 </div>
-             </div>
+  return (
+    <div>
+      <div className={styles.advancetext} style={collapsibleStyle}>
+        Advanced Options
+        <IconButton onClick={() => setOpen(!open)} aria-label="expand" size="small" style={iconStyle}>
+          {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+        </IconButton>
+      </div>
+      <div style={lineStyle}></div>
+      <div className={styles.advanceoptionsformgrid}>
+        <Collapse in={open} timeout="auto" unmountOnExit>
+          <div className={styles.advanceoptionsformgrid}>
+            <div className={styles.advancedoptionscontent}>
+              {compoundMode === 'compound' && (
+                <RadioToggle
+                  options={isProd ? toggleOptions.filter((itr) => itr.value != 'task') : toggleOptions}
+                  onChange={setApiMode}
+                  value={isProd ? 'normal' : apiMode}
+                />
+              )}
+              <div className={styles.selectCSS}>
+                <div>
+                  <div
+                    className={styles.labelwithinfo}
+                    onMouseOver={() => infoHandleHover('infoHover', description.simple_precursors)}
+                  >
+                    <label className={styles.label}>Use Simplified Precursor Library</label>
+                    <InfoImage imagePath={logo} altText="Info" information={description.simple_precursors} />
+                  </div>
+                  <input
+                    className={styles.inputfield}
+                    type="number"
+                    step="any"
+                    defaultValue={0}
+                    {...register('simple_precursors', { valueAsNumber: true })}
+                  />
+                </div>
+              </div>
+              <div className={styles.selectCSS}>
+                <div>
+                  <div className={styles.labelwithinfo}>
+                    <label className={styles.label}>Surface energy scaling factor</label>
+                  </div>
+                  <input
+                    className={styles.inputfield}
+                    type="number"
+                    step="any"
+                    defaultValue={0.12484}
+                    {...register('sigma', { valueAsNumber: true })}
+                  />
+                </div>
+              </div>
+              <div className={styles.selectCSS}>
+                <div>
+                  <div className={styles.labelwithinfo}>
+                    <label className={styles.label}>Transport barrier</label>
+                  </div>
+                  <input
+                    className={styles.inputfield}
+                    type="number"
+                    step="any"
+                    defaultValue={10}
+                    {...register('transport_constant', { valueAsNumber: true })}
+                  />
+                </div>
+              </div>
+              <div className={styles.selectCSS}>
+                <div>
+                  <div
+                    className={styles.labelwithinfoforselect}
+                    onMouseOver={() => infoHandleHover('infoHover', description.exclude_compositions)}
+                  >
+                    <label className={styles.label}>Formulas to filter out</label>
+                    <InfoImage imagePath={logo} altText="Info" information={description.exclude_compositions} />
+                  </div>
+                  <MultiSelect placeholder="Formulas to filter out" setValues={setExcludeCompositions} />
+                </div>
+              </div>
+            </div>
+            <div className={styles.Checkboxes}>
+              <div>
+                <div
+                  className={styles.labelwithinfo}
+                  onMouseOver={() => infoHandleHover('infoHover', description.confine_competing_to_icsd)}
+                >
+                  <FormCheckbox name="confine_competing_to_icsd" control={control} defaultValue={false} />
+                  <label className={styles.checklabel}>ICSD-based parasitic phases only</label>
+                  <InfoImage imagePath={logo} altText="Info" information={description.confine_competing_to_icsd} />
+                </div>
+              </div>
+              <div>
+                <div className={styles.labelwithinfo}>
+                  <FormCheckbox name="display_peroxides" control={control} defaultValue={true} />
+                  <label> Show reactions involving peroxides</label>
+                </div>
+              </div>
+              <div>
+                <div className={styles.labelwithinfo}>
+                  <FormCheckbox name="display_superoxides" control={control} defaultValue={false} />
+                  <label> Show reactions involving superoxides</label>
+                </div>
+              </div>
+              <div>
+                <div className={styles.labelwithinfo}>
+                  <FormCheckbox name="add_pareto" control={control} defaultValue={true} />
+                  <label> Show the Pareto front</label>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Collapse>
+      </div>
+    </div>
   );
-}
+};
 
 export default AdvancedOptions;
