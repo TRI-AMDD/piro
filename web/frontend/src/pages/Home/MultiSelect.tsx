0bb47a319b093cfd0ee02bf11f035ff753bc3443
Nadimpalli Varma (Contractor) <nadimpalli.varma.ctr@tri.global>
2024-02-22 17:10:52 +0000

    checking

--- web/frontend/src/pages/Home/MultiSelect.tsx
+++ web/frontend/src/pages/Home/MultiSelect.tsx
@@ -4,21 +4,6 @@ import styles from './Home.module.css';
 import { Optionselect } from './TypeProps';
 import { Dispatch, SetStateAction } from 'react';
 
-const customStyles = {
-  control: (base: CSSObject) => ({
-    ...base,
-    borderColor: '#757575', // default border color
-    '&:hover': {
-      borderColor: '#212121',
-      border: '2px solid #212121' // border color on hover
-    },
-    boxShadow: 'none' // remove default box shadow
-  })
-};
-type CSSObject = {
-  [key: string]: string | number | CSSObject;
-};
-
 interface Props {
   placeholder?: string;
   setValues: Dispatch<SetStateAction<MultiValue<Optionselect>>>;
@@ -31,7 +16,6 @@ export default function MultiSelect(props: Props) {
   return (
     <div className={styles.Multi}>
       <CreatableSelect
-        styles={customStyles}
         placeholder={placeholder}
         isMulti
         onChange={(newValues) => setValues(newValues)}
