be65f85fd3efbec60642f2616b3043cec41ebc97
Nadimpalli Varma (Contractor) <nadimpalli.varma.ctr@tri.global>
2024-02-22 10:26:27 +0000

    types added

--- web/frontend/src/pages/Home/MultiSelect.tsx
+++ web/frontend/src/pages/Home/MultiSelect.tsx
@@ -5,14 +5,34 @@ import styles from './Home.module.css';
 import { Optionselect } from './TypeProps';
 import { Dispatch, SetStateAction } from 'react';
 
-interface Props {
-  placeholder?: string;
-  setValues: Dispatch<SetStateAction<MultiValue<Optionselect>>>;
-}
 interface OptionType {
   label: string;
   value: string;
 }
+
+type CustomStyles = {
+  control: (
+    base: CSSObjectWithLabel,
+    props: ControlProps<OptionType, boolean, GroupBase<OptionType>>
+  ) => CSSObjectWithLabel;
+};
+
+const customStyles: CustomStyles = {
+  control: (base, props) => ({
+    ...base,
+    boxShadow: props.isFocused ? 'none' : base.boxShadow,
+    borderColor: props.isFocused ? '#ccc' : base.borderColor,
+    '&:hover': {
+      borderColor: props.isFocused ? '#212121' : base.borderColor,
+      border: props.isFocused ? '2px solid' : base.borderColor
+    }
+  })
+};
+interface Props {
+  placeholder?: string;
+  setValues: Dispatch<SetStateAction<MultiValue<Optionselect>>>;
+}
+
 export default function MultiSelect(props: Props) {
   const { placeholder, setValues } = props;
   const emptyOptions: Optionselect[] = [];
@@ -20,17 +40,7 @@ export default function MultiSelect(props: Props) {
   return (
     <div className={styles.Multi}>
       <CreatableSelect
-        styles={{
-          control: (base: CSSObjectWithLabel, props: ControlProps<OptionType, boolean, GroupBase<OptionType>>) => ({
-            ...base,
-            boxShadow: props.isFocused ? 'none' : base.boxShadow,
-            borderColor: props.isFocused ? '#ccc' : base.borderColor,
-            '&:hover': {
-              borderColor: props.isFocused ? '#212121' : base.borderColor,
-              border: props.isFocused ? '2px solid' : base.borderColor
-            }
-          })
-        }}
+        styles={customStyles}
         placeholder={placeholder}
         isMulti
         onChange={(newValues) => setValues(newValues)}
