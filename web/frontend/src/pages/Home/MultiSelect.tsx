465818eb0da6f0f9ae5e7bfffd332a0dd05fdfab
Nadimpalli Varma (Contractor) <nadimpalli.varma.ctr@tri.global>
2024-02-22 11:31:08 +0000

    reverted multiselect

--- web/frontend/src/pages/Home/MultiSelect.tsx
+++ web/frontend/src/pages/Home/MultiSelect.tsx
@@ -1,31 +1,9 @@
 import CreatableSelect from 'react-select/creatable';
-import { ControlProps, CSSObjectWithLabel, GroupBase } from 'react-select';
 import { MultiValue } from 'react-select';
 import styles from './Home.module.css';
 import { Optionselect } from './TypeProps';
 import { Dispatch, SetStateAction } from 'react';
 
-interface OptionType {
-  label: string;
-  value: string;
-}
-
-type CustomStyles = {
-  control: (
-    base: CSSObjectWithLabel,
-    props: ControlProps<OptionType, boolean, GroupBase<OptionType>>
-  ) => CSSObjectWithLabel;
-};
-
-const customStyles: CustomStyles = {
-  control: (base) => ({
-    ...base,
-    '&:hover': {
-      border: '1px solid #ff8b67',
-      boxShadow: '0px 0px 6px #ff8b67'
-    }
-  })
-};
 interface Props {
   placeholder?: string;
   setValues: Dispatch<SetStateAction<MultiValue<Optionselect>>>;
@@ -38,7 +16,6 @@ export default function MultiSelect(props: Props) {
   return (
     <div className={styles.Multi}>
       <CreatableSelect
-        styles={customStyles}
         placeholder={placeholder}
         isMulti
         onChange={(newValues) => setValues(newValues)}
