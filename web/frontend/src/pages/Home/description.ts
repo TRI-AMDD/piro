909b88839da1ccb29c2f1a7da45d3b1feec5fd35
N-S-R-Pradeep-varma <pradeepvarmanadimpalli333@gmail.com>
2024-02-29 15:53:41 +0530

    checking github account

--- web/frontend/src/pages/Home/description.ts
+++ web/frontend/src/pages/Home/description.ts
@@ -3,7 +3,7 @@ export const description = {
   max_component_precursors:
     'Used to limit the reactants to simpler sub-chemistries<br> of our target to obtain a refined precursor list.<br>`<br> For example, setting this as 2 for a ternary target compound <br> would limit precursors to binary compounds<br> (additional element count ignored)',
   flexible_competition:
-    'Used to limit the search for parasitic reactions to the <br> specified amount less than the target number<br> of elements for a given synthesis target.<br>`<br> For example, choosing 1 when analyzing a ternary target <br>would include binary phase products when <br> counting parasitic reactions',
+    'Used to limit the search for parasitic reactions to the<br> specified amount less than the target number<br> of elements for a given synthesis target.<br>`<br> For example, choosing 1 when analyzing a ternary target <br>would include binary phase products when <br> counting parasitic reactions',
   hull_distance:
     'Deselect “Stable precursors only” checkbox<br> if you wish to edit “Distance to Hull”.<br>`<br>This defines the energy range of metastable materials<br> for inclusion in precursor library.',
   add_elements: 'For example, to include carbonates and carbides, select C here.',
