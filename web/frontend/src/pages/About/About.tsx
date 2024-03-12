2b035fc54786529e7be0c1b199c3df236a057e37
Mike Puzon <100808191+mike-puzon-tri@users.noreply.github.com>
2024-03-12 09:54:17 -0700

    text changes (#169)

--- web/frontend/src/pages/About/About.tsx
+++ web/frontend/src/pages/About/About.tsx
@@ -58,8 +58,8 @@ const About: React.FC<highlightProps> = ({ highlightedParagraph, highlight }) =>
               It is a recommendation system for navigation and planning of synthesis of inorganic materials based on
               classical nucleation theory and semi-empirical, data-driven approximations to its parts. Currently it
               works with Materials Project data via its Rester API.
-              <br />
-              <br />
+            </p>
+            <p className={styles.Content}>
               Piro creates synthesis reaction planning plots for target polymorphs under a specific set of thermodynamic
               conditions and a precursor library, where favorable routes are those that are (nearly) Pareto optimal in
               terms of two metrics: nucleation barrier and phase-selection. It allows retrosynthetic analysis of target
@@ -75,7 +75,8 @@ const About: React.FC<highlightProps> = ({ highlightedParagraph, highlight }) =>
             <p className={styles.Content}>
               The Piro platform is developed by a team of researchers and software developers at Toyota Research
               Institute: Murat Aykol, Joseph Montoya, Jens Hummelshøj, Chris Fajardo, Michael Puzon, and Reko Ong.
-              <br />
+            </p>
+            <p className={styles.Content}>
               If you have any questions or feedback, please reach out to us at{' '}
               <u>
                 <a href="mailto:em-piro@tri.global" className={styles.mailstyle}>
@@ -93,13 +94,16 @@ const About: React.FC<highlightProps> = ({ highlightedParagraph, highlight }) =>
             <p className={styles.Content}>
               If you’d like to use the Python interface to Piro, or to access the code for this site, please visit our
               GitHub page:{' '}
-              <u>
-                <a href="https://github.com/TRI-AMDD/piro" target="_blank" rel="noreferrer">
-                  https://github.com/TRI-AMDD/piro
-                </a>
-              </u>
+              <a href="https://github.com/TRI-AMDD/piro" target="_blank" rel="noreferrer" className={styles.blackLink}>
+                https://github.com/TRI-AMDD/piro
+              </a>
               . Tutorial Jupyter notebooks showing how to use Piro locally are provided in the{' '}
-              <a href="https://github.com/TRI-AMDD/piro/tree/main/piro/notebooks" target="_blank" rel="noreferrer">
+              <a
+                href="https://github.com/TRI-AMDD/piro/tree/main/piro/notebooks"
+                target="_blank"
+                rel="noreferrer"
+                className={styles.blackLink}
+              >
                 <u>notebooks folder</u>
               </a>
               .<div className={styles.spacer}></div>
@@ -112,13 +116,16 @@ const About: React.FC<highlightProps> = ({ highlightedParagraph, highlight }) =>
             <h4 className={styles.Titles}>Manuscript</h4>
             <p className={styles.Content}>
               For more information about the physical theory behind Piro, please read our paper, “Rational Solid-State
-              Synthesis Routes for Inorganic Materials” by Murat Aykol et al{' '}
-              <u>
-                <a href="https://pubs.acs.org/doi/abs/10.1021/jacs.1c04888" target="_blank" rel="noreferrer">
-                  (https://pubs.acs.org/doi/abs/10.1021/jacs.1c04888)
-                </a>
-              </u>
-              .<div className={styles.spacer}></div>
+              Synthesis Routes for Inorganic Materials” by Murat Aykol et al. (
+              <a
+                href="https://pubs.acs.org/doi/abs/10.1021/jacs.1c04888"
+                target="_blank"
+                rel="noreferrer"
+                className={styles.blackLink}
+              >
+                https://pubs.acs.org/doi/abs/10.1021/jacs.1c04888
+              </a>
+              ).<div className={styles.spacer}></div>
             </p>
           </div>
         </div>
